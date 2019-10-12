3700c93c1f9fc61b46e5846b092b9699d473dcd6
武奇 <risky5200@gmail.com>
2019-10-12 11:47:56 +0800

    优化数据操作出错的提示

--- src/main/java/com/wuqi/auction/advice/ExceptionResolver.java
+++ src/main/java/com/wuqi/auction/advice/ExceptionResolver.java
@@ -3,10 +3,15 @@ package com.wuqi.auction.advice;
 import com.wuqi.auction.exception.AuctionException;
 import com.wuqi.auction.utils.ResultWrapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 创建人: 武奇
@@ -22,6 +27,20 @@ public class ExceptionResolver {
         return ResponseEntity.status(e.getCode())
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .body(ResultWrapper.error(e.getCode(), e.getMessage()));
+    }
+
+    @ExceptionHandler(SQLException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResultWrapper handleDBException(SQLException e) {
+        log.warn("数据库出错, 错误码: {}", e.getErrorCode());
+        log.warn("原因为: ", e);
+        switch (e.getErrorCode()) {
+            case 1062:
+                return ResultWrapper.error("数据重复");
+            case 1451:
+                return ResultWrapper.error("外键约束问题");
+        }
+        return ResultWrapper.error("数据操作失败");
     }
 
 }
