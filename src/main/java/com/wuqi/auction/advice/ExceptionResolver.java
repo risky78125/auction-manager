5bcea0e71fa1a847be390206704df339163c7ed1
武奇 <risky5200@gmail.com>
2019-10-12 11:10:54 +0800

    优化异常处理逻辑

--- src/main/java/com/wuqi/auction/advice/ExceptionResolver.java
+++ src/main/java/com/wuqi/auction/advice/ExceptionResolver.java
@@ -2,6 +2,7 @@ package com.wuqi.auction.advice;
 
 import com.wuqi.auction.exception.AuctionException;
 import com.wuqi.auction.utils.ResultWrapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,10 +13,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * 创建事件: 2019/10/12
  */
 @RestControllerAdvice
+@Slf4j
 public class ExceptionResolver {
 
     @ExceptionHandler(AuctionException.class)
     public ResponseEntity<ResultWrapper> handleInterruptException(AuctionException e) {
+        log.error("必须解决的异常: ", e);
         return ResponseEntity.status(e.getCode())
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .body(ResultWrapper.error(e.getCode(), e.getMessage()));
